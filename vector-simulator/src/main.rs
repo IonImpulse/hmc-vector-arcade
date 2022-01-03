b16f13d52fef15c02f79a43ee4bdfe76ff78c221
Ethan <edv121@outlook.com>
2022-01-03 13:56:05 -0800

    translated to 0 0 being middle

--- vector-simulator/src/main.rs
+++ vector-simulator/src/main.rs
@@ -82,36 +82,42 @@ impl VLine {
         self.brightness == 0
     }
 
-    pub fn distance(&self) -> f32 {
-        (((self.x2 - self.x1).powf(2.)) + ((self.y2 - self.y1).powf(2.))).sqrt()
+    pub fn as_canvas_coords(&self, canvas_width: f32, canvas_height: f32) -> (f32, f32, f32, f32) {
+        let x1 = ((self.x1 / MAX_COORD) * canvas_width as f32) + (canvas_width / 2.);
+        let x2 = ((self.x2 / MAX_COORD) * canvas_width as f32) + (canvas_width / 2.);
+
+        let y1 = ((-self.y1 / MAX_COORD) * canvas_height as f32) + (canvas_height / 2.);
+        let y2 = ((-self.y2 / MAX_COORD) * canvas_height as f32) + (canvas_height / 2.);
+
+        (x1, y1, x2, y2)
     }
 
-    pub fn calculate_draw_time(&self, speed: f32) -> f32 {
-        self.distance() / speed
+    pub fn distance(&self, canvas_width: f32, canvas_height: f32) -> f32 {
+        let coords = self.as_canvas_coords(canvas_width, canvas_height);
+        (((coords.2 - coords.0).powf(2.)) + ((coords.3 - coords.1).powf(2.))).sqrt()
+    }
+
+    pub fn calculate_draw_time(&self, speed: f32, canvas_width: f32, canvas_height: f32) -> f32 {
+        self.distance(canvas_width, canvas_height) / speed
     }
 
     pub fn get_progress(&self) -> f32 {
         self.draw_progress
     }
 
-    pub fn update_progress(&mut self, last_frame_time: Instant, speed: f32) -> f32 {
+    pub fn update_progress(&mut self, last_frame_time: Instant, speed: f32, canvas_width: f32, canvas_height: f32) -> f32 {
         if self.draw_progress != 1. {
             self.draw_progress =
-                (1. as f32).min(speed * last_frame_time.elapsed().as_secs_f32() / self.distance());
+                (1. as f32).min(speed * last_frame_time.elapsed().as_secs_f32() / self.distance(canvas_width, canvas_height));
         }
         self.draw_progress
     }
 }
 
-/* Constants
-    These will be used to provide the real world values for the application.
-
-    The speed of the motors/vector generator may vary
-    Speed is in pixels per second
-
-    Fade rate is how long in seconds it takes to decrease *one* brightness value
-    Brightness can be any integer value between 0 and 255
-*/
+/// Max value for signed 12-bit integer.
+const MAX_COORD: f32 = 4095.0;
+/// Min value for signed 12-bit integer.
+const MIN_COORD: f32 = -4095.0;
 
 fn open_file(path: &str) -> std::io::Result<Vec<VCommand>> {
     let mut file = std::fs::File::open(path)?;
@@ -140,7 +146,7 @@ fn open_file(path: &str) -> std::io::Result<Vec<VCommand>> {
 }
 
 fn main() {
-    // Speed of x/y motors in pixels per second
+    // Speed of x/y motors in coords per second
     let mut speed_of_motor: f32 = 1500.0;
     // Seconds it takes to fade one brightness value
     let mut fade_rate: f32 = 0.01;
@@ -179,7 +185,29 @@ fn main() {
                 .takes_value(true)
                 .help("Frame delay in seconds. Default is 0.5"),
         )
+        .arg(
+            Arg::with_name("show_axis")
+                .short("a")
+                .long("show-axis")
+                .takes_value(false)
+                .help("Show the axis"),
+        )
+        .arg(
+            Arg::with_name("realistic_mode")
+                .short("r")
+                .long("realistic-mode")
+                .takes_value(false)
+                .help("Sets speed, fade, and delay to realistic values. Is overridden by speed, fade, and delay arguments"),
+        )
         .get_matches();
+
+    let show_axis = matches.is_present("show_axis");
+
+    if matches.is_present("realistic_mode") {
+        speed_of_motor = 150000.0;
+        fade_rate = 0.001;
+        frame_delay = 1. / 30.;
+    }
 
     if let Some(speed) = matches.value_of("speed") {
         speed_of_motor = speed.parse::<f32>().expect("Speed must be a number");
@@ -208,7 +236,7 @@ fn main() {
     let window_size = glutin::dpi::PhysicalSize::new(800, 800);
     let el = EventLoop::new();
     let wb = WindowBuilder::new()
-        .with_inner_size(glutin::dpi::PhysicalSize::new(1000, 600))
+        .with_inner_size(glutin::dpi::PhysicalSize::new(800, 800))
         .with_title("Vector Generator");
 
     let windowed_context = ContextBuilder::new()
@@ -258,6 +286,11 @@ fn main() {
                     Color::rgbf(0.0, 0.0, 0.0),
                 );
 
+                // Draw ui
+                if show_axis {
+                    draw_ui(&mut canvas);
+                }
+
                 // Get current time
                 let now = Instant::now();
 
@@ -265,7 +298,7 @@ fn main() {
                 line_is_completed = true;
 
                 for line in drawn_lines.iter_mut() {
-                    let progress = line.update_progress(last_frame_time, speed_of_motor);
+                    let progress = line.update_progress(last_frame_time, speed_of_motor, canvas.width() as f32, canvas.height() as f32);
                     if progress != 1. {
                         line_is_completed = false;
                     } else {
@@ -331,8 +364,46 @@ fn main() {
     });
 }
 
+fn draw_ui<T: Renderer>(canvas: &mut Canvas<T>) {
+    let mut r = || {
+        // Draw axis lines as a dark blue
+        let mut p = Path::new();
+
+        let mut paint = Paint::color(Color::rgbf(
+            0.0,
+            0.0,
+            0.5,
+        ));
+
+        paint.set_line_width(1.);
+        paint.set_anti_alias(true);
+        paint.set_line_cap(LineCap::Round);
+        p.move_to(canvas.width() / 2., 0.);
+        p.line_to(canvas.width() / 2., canvas.height());
+        p.move_to(0., canvas.height() / 2.);
+        p.line_to(canvas.width(), canvas.height() / 2.);
+
+        canvas.stroke_path(&mut p, paint);
+    };
+
+    r();
+}
+
 fn draw_vector_line<T: Renderer>(canvas: &mut Canvas<T>, line: &VLine) {
     let mut r = |x1: f32, y1: f32, x2: f32, y2: f32, brightness: u8| {
+        // First, translate x/y to screen coordinates
+        // knowing that the origin is in the top left corner
+        // and the y axis is inverted
+
+        // Shift over according to the canvas size
+        // and max/min 12 bit values
+        let translated_coords = line.as_canvas_coords(canvas.width() as f32, canvas.height() as f32);
+
+        let x1 = translated_coords.0;
+        let y1 = translated_coords.1;
+        let x2 = translated_coords.2;
+        let y2 = translated_coords.3;
+
         let mut p = Path::new();
         // Draw line according to the progress of the line
         let progress = line.get_progress();
@@ -344,10 +415,10 @@ fn draw_vector_line<T: Renderer>(canvas: &mut Canvas<T>, line: &VLine) {
         canvas.translate(x1, y1);
         canvas.rotate(angle);
         p.move_to(0., 0.);
-        p.line_to(line.distance() * progress, 0.);
+        p.line_to(line.distance(canvas.width() as f32, canvas.height() as f32) * progress, 0.);
         let mut paint = Paint::color(Color::rgbf(
             brightness as f32 / 500.,
-            brightness as f32 / 500.,
+            brightness as f32 / 255.,
             brightness as f32 / 255.,
         ));
         paint.set_line_width(2.);
