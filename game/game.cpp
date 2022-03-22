301560c7045d4360b679a96c020f213d2c395754
Adam B <adamrbeckwith@gmail.com>
2022-03-22 09:32:10 -0700

    Moving square example

--- game/game.cpp
+++ game/game.cpp
@@ -4,9 +4,18 @@
 #include <string>
 #include <bitset>
 #include "wrapper.cpp"
+#include <conio.h>
 
-#define FRAME_DELAY .3
 
+#define FRAME_DELAY .01
+
+// using namespace System;
+using namespace std;
+
+
+
+int box_x = 0;
+int box_y = 0;
 
 
 typedef struct {
@@ -22,14 +31,16 @@ typedef struct {
 } Object;
 
 void doNextFrame() {
-    int box_x = 0;
-    int box_y = 0;
+
     draw_buffer_switch();  
     absolute_vec(box_x, box_y, 0);
     absolute_vec(box_x + 100, box_y, 1023);
     absolute_vec(box_x + 100, box_y + 100, 1023);
     absolute_vec(box_x, box_y + 100, 1023);
     absolute_vec(box_x, box_y, 1023);
+    absolute_vec(-250,-10,0);
+    absolute_vec(250,1-10,255);
+
     halt();
     // std::cout << "0111"  << std::endl;
     // std::cout << "10000000000000000000000000000000"  << std::endl;
@@ -52,6 +63,33 @@ int main() {
         auto start = Clock::now();
         doNextFrame();
         auto end = Clock::now();
+
+       
+
+        if(kbhit())
+        {
+            int input = _getch();
+            if (input == 100){ //pressed d 
+                box_x = box_x + 10;
+            }
+             if (input == 115){ //pressed s
+                box_y = box_y - 10;
+            }
+             if (input == 119){ //pressed w
+                box_y = box_y + 10;
+            }
+            if (input == 97){ //pressed a
+                box_x = box_x - 10;
+            }
+            
+           
+
+            if (box_x > 256) {
+                box_x = -356;
+            }
+        }
+        
+
         std::chrono::duration<double> frameTimeObj = end - start;
         double frameTime = frameTimeObj.count();
         if (frameTime > FRAME_DELAY) {
